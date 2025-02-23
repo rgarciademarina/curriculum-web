116fe5b69f401aaa39bd4cafbb9ff957d02893a2
rgarciademarina <raulgmm@ext.inditex.com>
2025-02-23 15:46:08 +0100

    chore - move experience

---
+++ js/experience-data.js
@@ -0,0 +1,52 @@
+export const experienceData = [
+    {
+        period: "2021 - current",
+        title: "Senior QA Automation Engineer",
+        company: "Kairos DS",
+        techStack: ["Java", "Selenium", "Cucumber", "TestNG", "Jenkins", "Git"],
+        details: [
+            "experience.kairos.detail1",
+            "experience.kairos.detail2",
+            "experience.kairos.detail3",
+            "experience.kairos.detail4"
+        ]
+    },
+    {
+        period: "Agosto 2016 - Marzo 2017",
+        title: "QA Engineer",
+        company: "Steelmood for Carrefour",
+        techStack: ["Java", "Selenium", "Rest Assured", "Cucumber", "Jira"],
+        details: [
+            "experience.steelmood.detail1",
+            "experience.steelmood.detail2",
+            "experience.steelmood.detail3",
+            "experience.steelmood.detail4",
+            "experience.steelmood.detail5"
+        ]
+    },
+    {
+        period: "Abril 2015 - Agosto 2016",
+        title: "Tester",
+        company: "T-Systems for Allianz",
+        techStack: ["Silk Central", "Test Planning", "Functional Testing"],
+        details: [
+            "experience.tsystems.detail1",
+            "experience.tsystems.detail2",
+            "experience.tsystems.detail3"
+        ]
+    },
+    {
+        period: "Enero 2015 - Abril 2015",
+        title: "Técnico de Roaming",
+        company: "Alalza",
+        techStack: ["Roaming", "VoIP", "LTE", "Network"],
+        details: [
+            "experience.alalza.detail1",
+            "experience.alalza.detail2",
+            "experience.alalza.detail3",
+            "experience.alalza.detail4",
+            "experience.alalza.detail5",
+            "experience.alalza.detail6"
+        ]
+    }
+];
